7f4267588bfa966849d9ccdd7eda821de9dc9d72
sabbir-mahmud <sabbir.mahmud.zim@gmail.com>
2022-04-07 18:08:30 +0600

    Sign in with email and password added

--- src/Components/LogIn/LogIn.js
+++ src/Components/LogIn/LogIn.js
@@ -1,18 +1,37 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
-import { getAuth, signInWithPopup, GoogleAuthProvider } from "firebase/auth";
+import { getAuth, signInWithPopup, GoogleAuthProvider, signInWithEmailAndPassword } from "firebase/auth";
 import app from '../../Firebase/Firebase.init';
 
 const LogIn = () => {
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
     // Google auth provider
     const provider = new GoogleAuthProvider();
     const auth = getAuth(app);
 
     // sign In navigate button
     const navigate = useNavigate();
-    const handleRegisterlink = () => {
-        navigate('/register');
+    const handleRegisterlink = () => navigate('/register');
+
+    // get email
+    const handleEmail = (e) => setEmail(e.target.value);
+
+    // get password
+    const handlePassword = (e) => setPassword(e.target.value);
+
+    // sign in button
+    const handleSignIn = (e) => {
+        e.preventDefault();
+        signInWithEmailAndPassword(auth, email, password)
+            .then((result) => {
+                console.log(result)
+            })
+            .catch((error) => console.error(error))
     }
+
+
+    // logIn with google
     const googleLogin = () => {
         signInWithPopup(auth, provider)
             .then(result => console.log(result))
@@ -25,20 +44,20 @@ const LogIn = () => {
                     <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="email">
                         Email
                     </label>
-                    <input className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="email" type="text" placeholder="Email" />
+                    <input onBlur={handleEmail} className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="email" type="text" placeholder="Email" />
                 </div>
                 <div className="mb-6">
                     <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="password">
                         Password
                     </label>
-                    <input className="shadow appearance-none border border-red-500 rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline" id="password" type="password" placeholder="******************" />
-                    <p className="text-red-500 text-xs italic">Please choose a password.</p>
+                    <input onBlur={handlePassword} className="shadow appearance-none  rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline" id="password" type="password" placeholder="******************" />
+                    <p className=" text-xs italic">Please choose a password.</p>
                 </div>
                 <a className="inline-block align-baseline font-bold text-sm text-blue-500 hover:text-blue-800" href="/">
                     Forgot Password?
                 </a>
                 <div className="mt-5 flex items-center">
-                    <button className="bg-blue-500 mr-7 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" type="button">
+                    <button onClick={handleSignIn} className="bg-blue-500 mr-7 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" type="button">
                         Sign In
                     </button>
                     <button onClick={handleRegisterlink} className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" type="button">
--- src/Components/Register/Register.js
+++ src/Components/Register/Register.js
@@ -59,8 +59,8 @@ const Register = () => {
                     <label className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" htmlFor="grid-first-name">
                         First Name
                     </label>
-                    <input onBlur={handleDisplayName} className="appearance-none block w-full bg-gray-200 text-gray-700 border border-red-500 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white" id="grid-first-name" type="text" placeholder="Jane" />
-                    <p className="text-red-500 text-xs italic">Please fill out this field.</p>
+                    <input onBlur={handleDisplayName} className="appearance-none block w-full bg-gray-200 text-gray-700  rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white" id="grid-first-name" type="text" placeholder="Jane" />
+                    <p className="text-xs italic">Please fill out this field.</p>
                 </div>
                 <div className="w-full md:w-1/2 px-3">
                     <label className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" htmlFor="grid-last-name">
